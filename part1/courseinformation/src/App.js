247ff0cbf32e12a42deeb945da613c41a95b6fc8
Robert <ridal.robert@gmail.com>
2022-06-08 01:27:14 +0300

    1.2: course information, step2

--- part1/courseinformation/src/App.js
+++ part1/courseinformation/src/App.js
@@ -1,19 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const courseItems = [
+    {
+      part: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1}/>
-      <Content part1={part2} exercises1={exercises2}/>
-      <Content part1={part3} exercises1={exercises3}/>
-      <Total total={(exercises1 + exercises2 + exercises3)}/>
+      <Header course={course} />
+      <Content parts={courseItems} />
+      <Total total={courseItems} />
     </div>
   )
 }
@@ -29,19 +35,29 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.parts[0].part + ' ' + props.parts[0].exercises} />
+      <Part part={props.parts[1].part + ' ' + props.parts[1].exercises} />
+      <Part part={props.parts[2].part + ' ' + props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part}</p>
     </div>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
     </div>
   )
 }
+
 
 export default App
