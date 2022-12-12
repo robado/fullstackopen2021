50c164915c01b6a0b1fd49ad624528f1e8a4014b
Robert <ridal.robert@gmail.com>
2022-12-13 00:24:32 +0200

    1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,41 +4,42 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
+// oikea paikka komponentin määrittelyyn
+const Statistics = (props) => {
+  // ...
+  const totalFeedback =  props.good + props.neutral + props.bad
+  const average =(props.good - props.bad) / totalFeedback
+  const positivePercent = props.good / totalFeedback * 100
+
+  return (
+    <div>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+      <div>All {totalFeedback}</div>
+      <div>Average {average}</div>
+      <div>Positive {positivePercent} %</div>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  // const [all, setAll] = useState(0)
-  // const [average, setAverage] = useState(0)
-  // const [positive, setPositive] = useState(0)
-  
-  const handleGood = () => setGood(good + 1)
-
-  const handleNeutral = () => setNeutral(neutral + 1)
-
-  const handleBad = () => setBad(bad + 1)
-
-  const totalFeedback =  good + neutral + bad
-
-  const average =(good - bad) / totalFeedback
-  
-  const positivePercent = good / totalFeedback * 100
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => handleGood()} text='good' />
-      <Button handleClick={() => handleNeutral()} text='neutral' />
-      <Button handleClick={() => handleBad()} text='bad' />
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h1>statictics</h1>
-      <div>good {good}</div>
+{/*       <div>good {good}</div>
       <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {totalFeedback}</div>
-      <div>average {average}</div>
-      <div>positive {positivePercent} %</div>
+      <div>bad {bad}</div> */}
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
