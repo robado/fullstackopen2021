1d0c726c9e56a9827b66d0792e49544832955594
Robert <ridal.robert@gmail.com>
2022-12-12 20:21:10 +0200

    1.7: unicafe step2... ugly code ;

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,15 +9,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const consolePrint = () => console.log('helloe')
-
+  
+  // const [all, setAll] = useState(0)
+  // const [average, setAverage] = useState(0)
+  // const [positive, setPositive] = useState(0)
+  
   const handleGood = () => setGood(good + 1)
 
   const handleNeutral = () => setNeutral(neutral + 1)
 
   const handleBad = () => setBad(bad + 1)
 
+  const totalFeedback =  good + neutral + bad
+
+  const average =(good - bad) / totalFeedback
+  
+  const positivePercent = good / totalFeedback * 100
 
   return (
     <div>
@@ -29,6 +36,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {totalFeedback}</div>
+      <div>average {average}</div>
+      <div>positive {positivePercent} %</div>
     </div>
   )
 }
