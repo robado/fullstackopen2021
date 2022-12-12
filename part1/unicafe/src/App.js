691f03372729294d2f706a65b3dbc54fc2a97545
robado <ridal.robert@gmail.com>
2022-12-12 15:15:53 +0000

    1.6: unicafe step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const consolePrint = () => console.log('helloe')
+
+  const handleGood = () => setGood(good + 1)
+
+  const handleNeutral = () => setNeutral(neutral + 1)
+
+  const handleBad = () => setBad(bad + 1)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => handleGood()} text='good' />
+      <Button handleClick={() => handleNeutral()} text='neutral' />
+      <Button handleClick={() => handleBad()} text='bad' />
+      <h1>statictics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
