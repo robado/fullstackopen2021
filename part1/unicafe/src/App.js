8bfeec03bb343a8b0ba42759f465cef45052fdac
robado <ridal.robert@gmail.com>
2022-12-13 10:05:00 +0000

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,8 +7,8 @@ const Button = ({ handleClick, text }) => (
 // oikea paikka komponentin määrittelyyn
 const Statistics = (props) => {
   // ...
-  const totalFeedback =  props.good + props.neutral + props.bad
-  const average =(props.good - props.bad) / totalFeedback
+  const totalFeedback = props.good + props.neutral + props.bad
+  const average = (props.good - props.bad) / totalFeedback
   const positivePercent = props.good / totalFeedback * 100
 
   if (totalFeedback == 0) {
@@ -18,21 +18,24 @@ const Statistics = (props) => {
   }
 
   return (
-    <div>
-      <StatisticLine text="Good" value={props.good}/>
-      <StatisticLine text="Neutral" value={props.neutral}/>
-      <StatisticLine text="Bad" value={props.bad}/>
-      <StatisticLine text="All" value={totalFeedback}/>
-      <StatisticLine text="Average" value={average}/>
-      <StatisticLine text="Positive" value={positivePercent}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="Good" value={props.good} />
+        <StatisticLine text="Neutral" value={props.neutral} />
+        <StatisticLine text="Bad" value={props.bad} />
+        <StatisticLine text="All" value={totalFeedback} />
+        <StatisticLine text="Average" value={average} />
+        <StatisticLine text="Positive" value={positivePercent} />
+      </tbody>
+    </table>
   )
 }
 
 const StatisticLine = (props) => {
-  console.log(props)
   return (
-    <div>{props.text} {props.value}</div>
+    <tr>
+      <td>{props.text} {props.value}</td>
+    </tr>
   )
 }
 
@@ -49,7 +52,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h1>statictics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
