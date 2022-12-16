cbbd548dc12fa827a46795dee22f8ba3fe80e6f9
robado <ridal.robert@gmail.com>
2022-12-16 13:35:42 +0200

    2.4: kurssitiedot step9 & 2.4: kurssitiedot step9

--- part2/kurssitiedot/src/components/Total.js
+++ part2/kurssitiedot/src/components/Total.js
@@ -1,5 +1,14 @@
 const Total = ({ parts }) => {
     const sum = parts.reduce((total, currentValue) => total + currentValue.exercises, 0);
+
+    /* With arrow function. Now console.log is possible to insert inside
+     const sum = parts.reduce((total, currentValue) => {
+        return total + currentValue.exercises
+    }, 0); */
+    /* Can insert console.log inside reduce
+    const sum1 = parts.reduce((total, currentValue) => console.log('Terve', total, currentValue) || total + currentValue, 0)
+    console.log('sum1...', sum1)
+    */
     
     return (
         <div>
